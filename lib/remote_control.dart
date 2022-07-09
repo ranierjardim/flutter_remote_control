cd5de1a932000fd0443adf5c72b2dead32441b63
Ranier Jardim <ranierjardim@gmail.com>
2022-07-09 14:11:53 -0300

    Saving work

--- lib/remote_control.dart
+++ lib/remote_control.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:flutter/services.dart';
-export 'package:remote_control_core/remote_control_core.dart';
+export 'package:remote_control/remote_control.dart';
 
 class RemoteControl {
   static const MethodChannel _channel = MethodChannel('remote_control');
